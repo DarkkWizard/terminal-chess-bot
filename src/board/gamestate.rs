fe5c2c83d8ca1ded24c31255839161c3412434c4
DarkkWizard <162348444+DarkkWizard@users.noreply.github.com>
2025-01-30 12:23:59 -0500

    all of em

---
+++ src/board/gamestate.rs
@@ -0,0 +1,10 @@
+use crate::defs::Side;
+
+pub struct Gamestate {
+    pub side_to_play: u8,
+}
+impl Gamestate {
+    pub fn new(side: u8) -> Gamestate {
+        Gamestate { side_to_play: side }
+    }
+}
---
+++ src/defs.rs
@@ -0,0 +1,6 @@
+pub struct Side;
+impl Side {
+    pub const WHITE: usize = 0;
+    pub const BLACK: usize = 1;
+    pub const BOTH: usize = 2;
+}
